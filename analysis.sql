8f926736b7ddf286e5e73175a88ae11936bafb7a
Nazamuddin <125904524+nazammm@users.noreply.github.com>
2025-07-03 00:24:14 +0700

    Segmentation
    Membagi customer menjadi 7 segment berdasarkan average:
    1. Elite
    2. Loyal Low Spend
    3. Potential
    4. Low Potential
    5. Inactive Elite
    6. Inactive Loyal
    7. Lost customer

--- analysis.sql
+++ analysis.sql
@@ -1,17 +1,13 @@
 WITH rfm_data AS (
   SELECT
     customer_id,
-    -- Hitung Recency (hari sejak transaksi terakhir)
     DATEDIFF(CURDATE(), MAX(order_date)) AS recency,
-    -- Hitung Frequency (total pesanan)
     COUNT(DISTINCT order_id) AS frequency,
-    -- Hitung Monetary (total nilai pembayaran)
     SUM(payment_value) AS monetary
   FROM e_commerce_transactions
   GROUP BY customer_id
 ),
 
--- Hitung nilai rata-rata RFM
 avg_values AS (
   SELECT
     AVG(recency) AS avg_recency,
@@ -19,4 +15,20 @@ avg_values AS (
     AVG(monetary) AS avg_monetary
   FROM rfm_data
 )
-SELECT * FROM avg_values;
+
+SELECT
+  r.customer_id,
+  r.recency,
+  r.frequency,
+  r.monetary,
+  CASE
+    WHEN r.recency < a.avg_recency AND r.frequency > a.avg_frequency AND r.monetary > a.avg_monetary THEN 'Elite'
+    WHEN r.recency < a.avg_recency AND r.frequency > a.avg_frequency AND r.monetary <= a.avg_monetary THEN 'Loyal Low Spend'
+    WHEN r.recency < a.avg_recency AND r.frequency <= a.avg_frequency AND r.monetary > a.avg_monetary THEN 'Potential'
+    WHEN r.recency < a.avg_recency AND r.frequency <= a.avg_frequency AND r.monetary <= a.avg_monetary THEN 'Low Potential'
+    WHEN r.recency >= a.avg_recency AND r.frequency > a.avg_frequency AND r.monetary > a.avg_monetary THEN 'Inactive Elite'
+    WHEN r.recency >= a.avg_recency AND r.frequency > a.avg_frequency AND r.monetary <= a.avg_monetary THEN 'Inactive Loyal'
+    ELSE 'Lost Customers'
+  END AS rfm_segment
+FROM rfm_data r
+CROSS JOIN avg_values a;
